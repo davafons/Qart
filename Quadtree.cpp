936c5952a0d0a41bf357974eb5fd7b9eb865558d
Dibad <dibadgithub@gmail.com>
2018-02-05 23:10:52 +0000

    Project simplified. Deleted VS files. Code cleared (Texture class deleted as it wasn't really necessary).

--- Quadtree.cpp
+++ Quadtree.cpp
@@ -1,107 +1,135 @@
-#include <algorithm>
+#include <iostream>
+#include <memory>
 #include <cmath>
 
 #include "Quadtree.h"
 
-Quadtree::Quadtree(Quadtree * parent, Texture & texture, int level, const SDL_Rect& rect) :
-	texture_(texture),
-	level_(level),
+
+Quadtree::Quadtree(std::unique_ptr<SDL_Surface> & image, int level, const SDL_Rect & rect) :
+	image_(image),
 	rect_(rect),
-	parent_(parent),
-	nodes_(std::vector<std::unique_ptr<Quadtree> >(4))
+	level_(level),
+	av_color_(SDL_Color()),
+	error_(0.0),
+	leafs_(std::vector<Quadtree>())
 {
-	average_color_ = texture_.getAverageColor(rect_);
-	calcError();
-	std::cout << "Node x: " << rect.x << " y: " << rect.y << " Error: " << error_ << std::endl;
+	calculateColorAndError();
 }
 
 
-Quadtree::~Quadtree()
+Quadtree::~Quadtree() { }
+
+
+double Quadtree::getError(void) const { return error_; }
+
+
+void Quadtree::clear(void) { leafs_.clear(); }
+
+
+void Quadtree::split(SDL_Renderer * renderer, bool outline)
 {
-	nodes_.clear();
+	// There is no need to check if the square is bigger than 1 (pixel), those will have 0.0 error when constructed
+
+	int subwidth = (float)rect_.w / 2 + 0.5;
+	int subheight = (float)rect_.h / 2 + 0.5;
+
+	leafs_.emplace_back(image_, level_ + 1, SDL_Rect{ rect_.x, rect_.y, subwidth , subheight  });								// Top left
+	leafs_.emplace_back(image_, level_ + 1, SDL_Rect{ rect_.x + rect_.w / 2, rect_.y, subwidth, subheight });					// Top right
+	leafs_.emplace_back(image_, level_ + 1, SDL_Rect{ rect_.x, rect_.y + rect_.h / 2, subwidth, subheight });					// Bot left
+	leafs_.emplace_back(image_, level_ + 1, SDL_Rect{ rect_.x + rect_.w / 2, rect_.y + rect_.h / 2, subwidth, subheight });		// Bot right
+
+	for (auto & leaf : leafs_)
+		leaf.render(renderer, outline);
 }
 
-SDL_Color Quadtree::getAverageColor(void) const
-{
-	return average_color_;
-}
 
-
-void Quadtree::split(void)
+Quadtree * Quadtree::findWorst(void)
 {
-	if (rect_.w > 1) // Can't subdivide a node that is 1 pixel width
+	if (leafs_.empty())
+		return this;
+
+	double worst_error = 0;
+	Quadtree * worst = nullptr;
+
+	for (auto & leaf : leafs_)
 	{
-		///std::cout << rect_.h / 2 << "Round" << (float)rect_.h / 2 + 0.5f << std::endl;
-		///std::cout << subwidth << " " << subheight << std::endl;
+		Quadtree * temp = leaf.findWorst(); // Recursive call
 
-		if (nodes_[0] != nullptr)
+		if (temp != nullptr)
 		{
-			int max = nodes_[0]->getError();
-			int ix = 0;
-			for (int i = 0; i < 4; ++i)
+			double temp_error = temp->getError();
+
+			if (temp_error > worst_error)
 			{
-				if (nodes_[i]->getError() > max)
-				{
-					max = nodes_[i]->getError();
-					ix = i;
-				}
+				worst_error = temp_error;
+				worst = temp;		
 			}
-			nodes_[ix]->split();
-
 		}
-		else 
+	}
+	return worst;
+}
+
+
+void Quadtree::render(SDL_Renderer * renderer, bool outline) const
+{
+	SDL_SetRenderDrawColor(renderer, av_color_.r, av_color_.g, av_color_.b, av_color_.a);
+	SDL_RenderFillRect(renderer, &rect_); 
+
+	if (outline) // Renders black borders if true
+	{
+		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF);
+		SDL_RenderDrawRect(renderer, &rect_);
+	}
+}
+
+
+void Quadtree::calculateColorAndError(void)
+{
+	int totalR = 0, totalG = 0, totalB = 0;
+
+	SDL_LockSurface(image_.get()); // Lock surface for pixel manipulation
+	
+	// Calculate average color
+	for (int y = rect_.y; y < rect_.y + rect_.h; ++y)
+	{
+		for (int x = rect_.x; x < rect_.x + rect_.w; ++x)
 		{
-			int subwidth = (float)rect_.w / 2 + 0.5;
-			int subheight = (float)rect_.h / 2 + 0.5;
+			Uint8 r, g, b;
 
-			nodes_[0] = std::make_unique<Quadtree>(this, texture_, level_ + 1, SDL_Rect{ rect_.x, rect_.y, subwidth, subheight });									// Top left
-			nodes_[1] = std::make_unique<Quadtree>(this, texture_, level_ + 1, SDL_Rect{ rect_.x + rect_.w / 2, rect_.y, subwidth, subheight });					// Top right
-			nodes_[2] = std::make_unique<Quadtree>(this, texture_, level_ + 1, SDL_Rect{ rect_.x, rect_.y + rect_.h / 2, subwidth, subheight });					// Bot left
-			nodes_[3] = std::make_unique<Quadtree>(this, texture_, level_ + 1, SDL_Rect{ rect_.x + rect_.w / 2, rect_.y + rect_.h / 2, subwidth, subheight });		// Bot right
-		
-			error_ = 0;
+			SDL_GetRGB(getPixel32(x, y), image_->format, &r, &g, &b);
+
+			totalR += r;
+			totalG += g;
+			totalB += b;
 		}
 	}
-}
 
+	av_color_ = { Uint8(totalR / (rect_.w * rect_.h)), Uint8(totalG / (rect_.w * rect_.h)), Uint8(totalB / (rect_.w * rect_.h)), 0xFF };
 
-void Quadtree::calcError(void)
-{
-	if (parent_ != nullptr)
+	// Calculate error (Difference between the average color and each pixel)
+	for (int y = rect_.y; y < rect_.y + rect_.h; ++y)
 	{
-		int re = abs(parent_->getAverageColor().r - average_color_.r);
-		int ge = abs(parent_->getAverageColor().g - average_color_.g);
-		int be = abs(parent_->getAverageColor().b - average_color_.b);
+		for (int x = rect_.x; x < rect_.x + rect_.w; ++x)
+		{
+			Uint8 r, g, b;
 
-		error_ = re + ge + be;
+			SDL_GetRGB(getPixel32(x, y), image_->format, &r, &g, &b);
+
+			error_ += std::pow((int)r - (int)av_color_.r, 2);
+			error_ += std::pow((int)g - (int)av_color_.g, 2);
+			error_ += std::pow((int)b - (int)av_color_.b, 2);
+		}
 	}
-	else
-		error_ = 100;
+
+	error_ /= rect_.w;
+
+	SDL_UnlockSurface(image_.get());
 }
 
-int Quadtree::getError(void) const
+
+Uint32 Quadtree::getPixel32(int x, int y) const
 {
-	return error_;
+	Uint8 * p = (Uint8 *)image_->pixels + y * image_->pitch + x * 4;
+
+	return *(Uint32 *)p;
 }
-
-
-
-void Quadtree::render(SDL_Renderer * renderer) const
-{
-	if (nodes_[0] == nullptr)
-	{
-		SDL_SetRenderDrawColor(renderer, average_color_.r, average_color_.g, average_color_.b, average_color_.a);
-		SDL_RenderFillRect(renderer, &rect_);
-	}
-	else
-	{
-		for (auto & node : nodes_)
-			node->render(renderer);
-	}
-}
-
-bool Quadtree::operator<(const Quadtree & quad) const
-{
-	return error_ < quad.error_;
-}
-
--- Quadtree.h
+++ Quadtree.h
@@ -1,42 +1,45 @@
 #pragma once
 
-#include <iostream>
 #include <vector>
-#include <memory>
 
 #include <SDL.h>
-
-#include "Texture.h"
-
 
 class Quadtree
 {
 private:
-	Texture & texture_; // Can't be null
-	SDL_Color average_color_;
 
-	SDL_Rect rect_; // Dimensions of the node
+	std::unique_ptr<SDL_Surface> & image_;
+	SDL_Rect rect_;
 
-	int level_; // Current level
-	int error_;
+	int level_;
 
-	Quadtree * parent_;
-	std::vector<std::unique_ptr<Quadtree> > nodes_;
+	SDL_Color av_color_;
+	double error_;
+
+	std::vector<Quadtree> leafs_;
 
 public:
-	Quadtree(Quadtree * parent, Texture & texture, int level, const SDL_Rect& rect);
-	~Quadtree();
 
-	SDL_Color getAverageColor(void) const;
+	Quadtree(std::unique_ptr<SDL_Surface> & image, int level, const SDL_Rect & rect);
+	
+	~Quadtree(void);
 
-	void split(void);
+	double getError(void) const;
 
-	void calcError(void);
+	void clear(void);
 
-	int getError(void) const;
+	// Create 4 new leafs
+	void split(SDL_Renderer * renderer, bool outline);
 
-	void render(SDL_Renderer * renderer) const;
+	// Loop through every leaf recursively finding the node with the worst error. Linear to the number of empty leafs in the whole tree
+	Quadtree * findWorst(void);
 
-	bool operator<(const Quadtree & quad) const;
+	void render(SDL_Renderer * renderer, bool outline) const;
+
+private:
+
+	void calculateColorAndError(void);
+
+	Uint32 getPixel32(int x, int y) const;
 };
 
